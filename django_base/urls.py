7ab58907de62996a6e46895607529c2fb26efa4c
luca citta <lucacitta@gmail.com>
2022-05-23 19:38:03 -0300

    templates fix

--- django_base/urls.py
+++ django_base/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 
-from django_base.views import saludo, despedida, fecha_actual, probandoTemplate
+from django_base.views import saludo, despedida, fecha_actual, probando_template
 
 
 urlpatterns = [
@@ -9,5 +9,5 @@ urlpatterns = [
     path('saludo/<nombre>/', saludo, name = 'saludo'),
     path('despedida/', despedida, name = 'despedida'),
     path('fecha_actual/', fecha_actual, name = 'fecha_actual'),
-    path('probando-template/',probandoTemplate, name = 'probando_template'),   
+    path('probando-template/', probando_template, name = 'probando_template'), 
 ]
--- django_base/views.py
+++ django_base/views.py
@@ -1,8 +1,8 @@
-from django.http import HttpResponse
-
 from datetime import datetime
 
-from django.template import Template, Context
+from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def saludo(request, nombre):
     return HttpResponse(f'Buenas tardes {nombre} :D')
@@ -15,17 +15,5 @@ def fecha_actual(request):
     mensaje = f'Hoy es {fecha} !!'
     return HttpResponse(mensaje)
 
-def probandoTemplate(self):
-
-    miHtml = open("C:/Users/Luca/Desktop/Programacion/CodeHouse/Django/ecommerce/templates/template_1.html")
-
-    plantilla = Template(miHtml.read()) #Se carga en memoria nuestro documento, template1   
-    ##OJO importar template y contex, con: from django.template import Template, Context
-
-    miHtml.close() #Cerramos el archivo
-
-    miContexto = Context() #EN este caso no hay nada ya que no hay parametros, IGUAL hay que crearlo
-
-    documento = plantilla.render(miContexto) #Aca renderizamos la plantilla en documento
-
-    return HttpResponse(documento)
+def probando_template(request):
+    return render(request, 'template_1.html', context = {})
