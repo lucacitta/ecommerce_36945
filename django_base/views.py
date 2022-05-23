21b4e0917a2b41eae055c1b1a66e2fe65c0b1b34
luca citta <lucacitta@gmail.com>
2022-05-22 21:18:45 -0300

    cahnges templates

--- django_base/views.py
+++ django_base/views.py
@@ -2,6 +2,10 @@ from django.http import HttpResponse
 
 from datetime import datetime
 
+from django.template import Template, Context
+
+#from django.shortcuts import render
+
 def saludo(request, nombre):
     return HttpResponse(f'Buenas tardes {nombre} :D')
 
@@ -12,3 +16,23 @@ def fecha_actual(request):
     fecha = datetime.now().date()
     mensaje = f'Hoy es {fecha} !!'
     return HttpResponse(mensaje)
+
+def probandoTemplate(self):
+
+    miHtml = open("C:/Users/Luca/Desktop/Programacion/CodeHouse/Django/ecommerce/templates/template_1.html")
+
+    plantilla = Template(miHtml.read()) #Se carga en memoria nuestro documento, template1   
+    ##OJO importar template y contex, con: from django.template import Template, Context
+
+    miHtml.close() #Cerramos el archivo
+
+    miContexto = Context() #EN este caso no hay nada ya que no hay parametros, IGUAL hay que crearlo
+
+    documento = plantilla.render(miContexto) #Aca renderizamos la plantilla en documento
+
+    return HttpResponse(documento)
+
+from django.shortcuts import render
+
+def probando_template(request):
+    return render(request, 'template_1.html', context = {})
