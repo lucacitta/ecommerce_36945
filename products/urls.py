dfd3c16890c05b7d8f98aeb6a31c851eb16065c6
luca citta <lucacitta@gmail.com>
2022-06-06 20:45:03 -0300

    create,delete,list views

--- products/urls.py
+++ products/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from products.views import list_products, create_products, search_products, detail_product, delete_product
+from products.views import List_products, Create_product, search_products, Detail_product, Delete_product
 
 urlpatterns =[
-    path('', list_products, name = 'products'),
+    path('', List_products.as_view(), name = 'list_products'),
 
-    path('create-products/', create_products, name = 'create_products'),
+    path('create-products/', Create_product.as_view(), name = 'create_products'),
     path('search-product/', search_products, name = 'search_products'),
-    path('detail-product/<int:pk>/', detail_product, name = 'detail_product'),
-    path('delete-product/<int:pk>/', delete_product, name = 'delete_product'),
+    path('detail-product/<int:pk>/', Detail_product.as_view(), name = 'detail_product'),
+    path('delete-product/<int:pk>/', Delete_product.as_view(), name = 'delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,67 +1,100 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.urls import reverse
 
 from products.models import Products
 from products.forms import Product_form
 
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+
+
+class List_products(ListView):
+    model = Products
+    template_name= 'products.html'
+    queryset = Products.objects.filter(is_active = True)
+
+class Detail_product(DetailView):
+    model = Products
+    template_name= 'detail_product.html'
+
+class Create_product(CreateView):
+    model = Products
+    template_name = 'create_products.html'
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('detail_product', kwargs={'pk':self.object.pk})
+
+class Delete_product(DeleteView):
+    model = Products
+    template_name = 'delete_product.html'
+
+    def get_success_url(self):
+        return reverse('list_products')
+
+
 
 # Create your views here.       
-def list_products(request):
-    products = Products.objects.all()
-    context = {'products':products}
-    return render(request, 'products.html', context=context)
+# def list_products(request):
+#     products = Products.objects.all()
+#     context = {'products':products}
+#     return render(request, 'products.html', context=context)
 
-def detail_product(request, pk):
-    try:
-        product = Products.objects.get(id=pk)
-        context = {'product':product}
-        return render(request, 'product_detail.html', context=context)
-    except:
-        context = {'error':'El Producto no existe'}
-        return render(request, 'products.html', context=context)
+# def detail_product(request, pk):
+#     try:
+#         product = Products.objects.get(id=pk)
+#         context = {'product':product}
+#         return render(request, 'product_detail.html', context=context)
+#     except:
+#         context = {'error':'El Producto no existe'}
+#         return render(request, 'products.html', context=context)
 
-def delete_product(request, pk):
-    try:
-        if request.method == 'GET':
-            product = Products.objects.get(id=pk)
-            context = {'product':product}
-        else:
-            product = Products.objects.get(id=pk)
-            product.delete()
-            context = {'message':'Producto eliminado correctamente'}
+# def create_products(request):
+#     if request.method == 'GET':
 
-        return render(request, 'delete_product.html', context=context)
+#         form = Product_form()
+#         context = {'form':form}
 
-    except:
-        context = {'error':'El Producto no existe'}
-        return render(request, 'delete_product.html', context=context)
+#         return render(request, 'create_products.html', context=context)
 
-def create_products(request):
-    if request.method == 'GET':
-
-        form = Product_form()
-        context = {'form':form}
-
-        return render(request, 'create_products.html', context=context)
-
-    elif request.method == 'POST':
+#     elif request.method == 'POST':
         
-        form = Product_form(request.POST)
-        if form.is_valid():
-            new_product = Products.objects.create(
-                name = form.cleaned_data['name'],
-                price = form.cleaned_data['price'],
-                description = form.cleaned_data['description'],
-                SKU = form.cleaned_data['SKU'],
-                is_active = form.cleaned_data['is_active'],
-            )
-            context = {'new_product':new_product}
-        else:
-            context = {'errors':form.errors}
-        return render(request, 'create_products.html', context = context)
+#         form = Product_form(request.POST)
+#         if form.is_valid():
+#             new_product = Products.objects.create(
+#                 name = form.cleaned_data['name'],
+#                 price = form.cleaned_data['price'],
+#                 description = form.cleaned_data['description'],
+#                 SKU = form.cleaned_data['SKU'],
+#                 is_active = form.cleaned_data['is_active'],
+#             )
+#             context = {'new_product':new_product}
+#         else:
+#             context = {'errors':form.errors}
+#         return render(request, 'create_products.html', context = context)
 
-    else:
-        return HttpResponse('Only GET and POST methods are allowed')
+#     else:
+#         return HttpResponse('Only GET and POST methods are allowed')
+
+
+
+# def delete_product(request, pk):
+#     try:
+#         if request.method == 'GET':
+#             product = Products.objects.get(id=pk)
+#             context = {'product':product}
+#         else:
+#             product = Products.objects.get(id=pk)
+#             product.delete()
+#             context = {'message':'Producto eliminado correctamente'}
+
+#         return render(request, 'delete_product.html', context=context)
+
+#     except:
+#         context = {'error':'El Producto no existe'}
+#         return render(request, 'delete_product.html', context=context)
+
+
 
 def search_products(request):
     products = Products.objects.filter(name__icontains=request.GET['search'])
