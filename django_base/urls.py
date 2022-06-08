68d8529bde8fd78d73b82ac3c628bd77b5c7626b
luca citta <lucacitta@gmail.com>
2022-06-08 19:52:10 -0300

    login

--- django_base/urls.py
+++ django_base/urls.py
@@ -1,21 +1,20 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django_base.views import saludo, index, fecha_actual, probando_template, contact
+from django_base.views import saludo, index, fecha_actual, probando_template, contact, login_view
 
 
 
 
 urlpatterns = [
     path('', index, name = 'index'),
-    
     path('admin/', admin.site.urls),
     path('saludo/<nombre>/', saludo, name = 'saludo'),
-
     path('fecha_actual/', fecha_actual, name = 'fecha_actual'),
     path('probando-template/', probando_template, name = 'probando_template'),
-
     path('contact/', contact, name = 'contacto'),
+    path('products/', include('products.urls')),
 
-    path('products/', include('products.urls'))
+
+    path('login/', login_view, name = 'login'),
 ]
--- django_base/views.py
+++ django_base/views.py
@@ -3,12 +3,54 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+
+def login_view(request):
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data = request.POST)
+
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                context = {'message':f'Bienvenido {username}!! :D'}
+                return render(request, 'index.html', context = context)
+            else:
+                context = {'errors':'No hay ningun usuario con esas credenciales!!!'}
+                form = AuthenticationForm()
+                return render(request, 'auth/login.html', context = context)
+        else:
+            errors = form.errors
+            form = AuthenticationForm()
+            context = {'errors':errors, 'form':form} 
+            return render(request, 'auth/login.html', context = context)
+
+    else:
+        form = AuthenticationForm()
+        context = {'form':form}
+        return render(request, 'auth/login.html', context = context)
+
+
+def index(request):
+    print(request.user)
+    print(request.user.is_authenticated)
+    return render(request, 'index.html')
+
+
+
+
+
+
+
 
 def saludo(request, nombre):
     return HttpResponse(f'Buenas tardes {nombre} :D')
 
-def index(request):
-    return render(request, 'index.html')
+
 
 def fecha_actual(request):
     fecha = datetime.now().date()
